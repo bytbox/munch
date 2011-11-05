44b8a601a7fb5b48b01b74b4042dc0cddcb48cc8
Scott Lawrence <bytbox@gmail.com>
2011-11-05 19:23:56 -0400

    Atom support mostly working (except for href parsing)

--- atom.go
+++ atom.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"encoding/hex"
 	"log"
+	"xml"
 )
 
-type AtomData struct {
-
-}
-
 type AtomFeed struct {
-
+	Title string
+	Entry []AtomItemData
 }
 
 type AtomItemData struct {
-
+	ID      string
+	Title   string
+	Updated string
+	Summary string
 }
 
 func readAtom(feed *Feed) {
@@ -23,12 +25,38 @@ func readAtom(feed *Feed) {
 	if err != nil {
 		log.Print("ERROR: ", err.Error())
 	}
-	_ = r.Body
+	reader := r.Body
+	feedData := AtomFeed{}
+	err = xml.Unmarshal(reader, &feedData)
+	if err != nil {
+		log.Print("ERROR: ", err.Error())
+		return
+	}
 
 	changed := false
+	for _, itemData := range feedData.Entry {
+		uid := itemData.ID
+		id := hex.EncodeToString([]byte(feed.Info.Name + uid))
+		_, ok := feed.Items[uid]
+		if !ok {
+			changed = true
+			t := parseTime(itemData.Updated)
+			item := &Item {
+				Feed: feed,
+				Title: itemData.Title,
+				GUID: uid,
+				URL: "",
+				Date: *t,
+				Desc: itemData.Summary,
+				Read: false,
+				ID: id,
+			}
+			feed.Items[uid] = item
+		}
+	}
 
 	if (changed) {
 		updates <- DoUpdate
-		// TODO
+		// TODO run some commands from Config?
 	}
 }
